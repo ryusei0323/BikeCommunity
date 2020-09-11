58705f18bcc7ec48506ad142b275a789a8833c50
vagrant <vagrant@localhost.localdomain>
2020-09-11 11:59:02 +0000

    [Add]routes

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,20 @@ Rails.application.routes.draw do
 	    registrations: 'customer/users/registrations',
 	    passwords: 'customer/users/passwords'
 	  }
+
+	namespace :admin do
+    resources :bikes
+    resources :manufacturers, except: [:new, :show, :destroy]
+    resources :posts, except: [:new, :create, :destroy]
+    resources :users, except: [:new, :create, :destroy]
+  end
+
+  namespace :customer do
+    resources :bikes, only: [:index, :show]
+    resources :favorites, only: [:index, :show, :create, :destroy]
+    resources :manufacturers, only: :index
+    resources :posts
+    resources :users, except: [:index, :new, :create, :destroy]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
