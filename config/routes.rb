1bb3f70ccf30bfe7cbc0375aef757daef0711a77
vagrant <vagrant@localhost.localdomain>
2020-09-11 07:21:22 +0000

    [Add]devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
-# frozen_string_literal: true
-
 Rails.application.routes.draw do
+
+	  devise_for :admin_users, controllers: {
+		  sessions: 'admin/admin_users/sessions',
+		  registrations: 'admin/admin_users/registrations',
+		  passwords: 'admin/admin_users/passwords'
+	  }
+  end
+
+
+	  devise_for :users, controllers: {
+	    sessions: 'customer/users/sessions',
+	    registrations: 'customer/users/registrations',
+	    passwords: 'customer/users/passwords'
+	  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
