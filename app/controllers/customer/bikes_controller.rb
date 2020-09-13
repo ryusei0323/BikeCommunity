eec513141581bb2930053a8f71739fa30b574a52
vagrant <vagrant@localhost.localdomain>
2020-09-13 05:50:32 +0000

    [Add]customer-bike

--- app/controllers/customer/bikes_controller.rb
+++ app/controllers/customer/bikes_controller.rb
@@ -1,9 +1,13 @@
 class Customer::BikesController < ApplicationController
 
   def index
+    @bikes = Bike.where(bike_status: true)
+    @manufacturers = Manufacturer.where(status: true)
   end
 
   def show
+    @bike = Bike.find(params[:id])
+    @manufacturers = Manufacturer.where(status: true)
   end
 
 end
