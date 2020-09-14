8b0bb4bce60f21e3e63598cccd25d15ceacb4e23
vagrant <vagrant@localhost.localdomain>
2020-09-14 10:11:56 +0000

    [Add]admin-post

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,12 +1,35 @@
 class Admin::PostsController < ApplicationController
 
   def index
+    @posts = Post.all
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      flash[:notice] = '投稿を更新しました'
+      redirect_to admin_post_path(@post.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to admin_post_path
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:post_title, :post_sentence, :post_image)
+  end
 end
