7d4cf38213ade95e2c9b26a2990b6562d1e46450
vagrant <vagrant@localhost.localdomain>
2020-09-27 15:03:07 +0000

    [Fix]customer-favorite-ajax

--- app/controllers/customer/favorites_controller.rb
+++ app/controllers/customer/favorites_controller.rb
@@ -2,11 +2,8 @@ class Customer::FavoritesController < ApplicationController
 
   def create
     @post = Post.find(params[:post_id])
-    #いいねボタンを連打しても1回しかいいね出来ない様に条件付与
-    unless @post.favorited_by?(current_user)
-      favorite = current_user.favorites.new(post_id: @post.id)
-      favorite.save
-    end
+    favorite = current_user.favorites.new(post_id: @post.id)
+    favorite.save
   end
 
   def destroy
